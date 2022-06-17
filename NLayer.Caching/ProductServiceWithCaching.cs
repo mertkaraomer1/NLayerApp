6ebc42911e9132abf13d76fec318b6298bfc07a0
Lenovo <Lenovo@DESKTOP-AVC9VTM>
2022-06-17 16:13:39 +0300

    fix Web

--- NLayer.Caching/ProductServiceWithCaching.cs
+++ NLayer.Caching/ProductServiceWithCaching.cs
@@ -71,11 +71,11 @@ namespace NLayer.Caching
             return Task.FromResult(product);
         }
 
-        public  Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductsWithCategory()
+        public  Task<List<ProductWithCategoryDto>> GetProductsWithCategory()
         {
             var products = _memoryCache.Get<IEnumerable<Product>>(CacheProductKey);
             var productWithCategoryDto=_mapper.Map<List<ProductWithCategoryDto>>(products);
-            return Task.FromResult(CustomResponseDto<List<ProductWithCategoryDto>>.Success(200, productWithCategoryDto));
+            return Task.FromResult(productWithCategoryDto);
         }
 
         public async Task RemoveAsync(Product entity)
---
+++ NLayer.Web/Modules/repoServiceModul.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnıtOfWorks;
+using NLayer.Repository.Repositories;
+using NLayer.Repository.UnıtOfWork;
+using NLayer.Service.Mapping;
+using NLayer.Service.Services;
+using System.Reflection;
+using Module = Autofac.Module;
+namespace NLayer.Web.Modules
+{
+    public class repoServiceModul:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterGeneric(typeof(GenericRepository<>))
+                .As(typeof(IGenericRepository<>))
+                .InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(Service<>))
+                .As(typeof(IService<>))
+                .InstancePerLifetimeScope();
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+
+
+            var apiAssembly = Assembly.GetExecutingAssembly();
+            var repoAssembly= Assembly.GetAssembly(typeof(AppContext));
+            var serviceAssembly= Assembly.GetAssembly(typeof(MapProfile));
+            builder.RegisterAssemblyTypes(apiAssembly,repoAssembly, serviceAssembly)
+                .Where(x=>x.Name.EndsWith("Repository")).AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly)
+               .Where(x => x.Name.EndsWith("Service")).AsImplementedInterfaces()
+               .InstancePerLifetimeScope();
+
+        }
+
+    }
+}
